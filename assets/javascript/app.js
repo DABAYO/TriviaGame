21671a1551556a059cf641f56ff004210062a340
DABAYO <dbayo2000@yahoo.com>
2018-01-04 17:41:13 -0500

    second commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+setTimeout(timeUp, 1000 * 60);
+
+    function timeUp() {
+    
+    $("#timer").append("<h2>Time's Up!</h2>");
+    alert(Time is up!);
+    }
+
+var correct= 0;
+var incorrect= 0;
+var questionsArray = [q1, q2, q3, q4, q5];
+
+
+
+
+
